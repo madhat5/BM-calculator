f23971b25d82fec45b09aeecfedc5fe723226ee4
David Malary <davidmalary@Davids-MacBook-Pro.local>
2020-02-04 13:52:50 -0500

    switched slider logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,38 @@
 // console.log("black math calculator");
 
 var rangeSlider = function () {
-    $('input[type="range"]').rangeslider({
-        rangeClass: 'rangeslider',
-        horizontalClass: 'rangeslider--horizontal',
-        fillClass: 'rangeslider__fill',
-        handleClass: 'rangeslider__handle',
+    // $('input[type="range"]').rangeslider({
+    //     rangeClass: 'rangeslider',
+    //     horizontalClass: 'rangeslider--horizontal',
+    //     fillClass: 'rangeslider__fill',
+    //     handleClass: 'rangeslider__handle',
 
-        // onSlide: function(position, value) {
-        //     assign value to HTML
-        // },
+    //     onSlide: function(position, value) {
+    //         console.log(value)
+    //     },
 
-        // onSlideEnd: function(position, value) {
-        //     return value + assign to object?
-        // }
+    //     // onSlideEnd: function(position, value) {
+    //     //     return value + assign to object?
+    //     // }
+    // });
+
+    var slider = $('.range-slider'),
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
+
+    slider.each(function () {
+
+        value.each(function () {
+            var value = $(this).next().attr('value');
+            $(this).html(value);
+            console.log(value) 
+        });
+
+        console.log(value.innerHTML) 
+
+        range.on('input', function () {
+            $(this).prev(value).html(this.value);
+        });
     });
 };
 
