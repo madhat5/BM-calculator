0ca0cb35f31c7a18e8681ba45b966537417c5456
David Malary <davidmalary@Davids-MacBook-Pro.local>
2020-02-05 14:34:36 -0500

    still debugging slider, cashflow base done

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 // console.log("black math calculator");
 
-
-
 // RANGE SLIDER
 var rangeSlider = function () {
       var slider = $('.range-slider'),
@@ -14,7 +12,12 @@ var rangeSlider = function () {
             var value = $(this).next().attr('value');
             $(this).html(value);
 
-
+            // onclick?
+            range.click(function() {
+                console.log(value.innerHTML)
+              });
+            console.log($('input[type=range]').val())
+            console.log($('.range-slider__value').val())
 
             // var curerntVal = $(this)[0].innerHTML;
             // console.log(value) 
@@ -35,10 +38,26 @@ rangeSlider();
 
 
 // CASHLOW HISTORY
+var decayRate = 12,
+    npv = 44000;
+
+var displayRes = function (decayRate, npv) {
+    $('#cashflow-list')
+        .append(' <li><span class="cashflow-list-left">' + decayRate + '%</span> &nbsp;<span class="cashflow-list-right">' + npv + '</span></li>')
+};
+
+displayRes(25, 60000)
+
 /*
 get res vals for 
     - decay rate (pull from slider)
     - net present val (NPV) (set temp val)
-assign res to html div
+assign res to html li (within ul)
 display new res in new lines
+
+
+function endRes (decayRate, npv) {
+    append as li to ul
+    <li><span>decay rate</span><span>npv</span></li>
+}
 */
